bca21b702c64dc5fe89955f1043a0e8a7a867838
armanKhosravi <45129262+armanKhosravi@users.noreply.github.com>
2020-06-23 10:46:38 +0200

    Update profitability.js

--- profitability.js
+++ profitability.js
@@ -48,7 +48,7 @@ function theoreticalProfits(a, g, b, t, strategy, totalPublished, initialPoint) 
                     ((a - ((1 - g) * (1 - a) * (1 - a) * a * (1 - a - a)) / ((1 + (1 - a) * a) * (1 - a - a) + (1 - a) * a)) * ((1 - a - a + (1 - a) * a * (1 - a - a) + (1 - a) * a) / ((1 - a) * (1 - a) * a + 1 - a - a))) * b / t;
             break;
         case 'l': // Lead Mining Strategy
-            var apparentHashrate = Math.floor((totalPublished + initialPoint) / 2016) < 1 ? q - (((1 - q) * q * (1 - q - q) * (1 - g)) / g) * ((1 - (1 - q) * (1 - g) * (2 / (1 + Math.sqrt(1 - 4 * (1 - g) * (1 - q) * q)))) / (1 - q + (1 - q) * q - q)) :
+            var apparentHashrate = Math.floor((totalPublished + initialPoint) / 2016) < 1 ? a * ((1 - a + (1 - a) * a - a * a) / (1 - a + (1 - a) * a - a)) - (((1 - a) * a * (1 - a - a) * (1 - g)) / (g)) * ((1 - (1 - a) * (1 - g) * (2 / (1 + Math.sqrt(1 - 4 * (1 - g) * (1 - a) * a)))) / (1 - a + (1 - a) * a - a)) :
                     a - (((1 - a) * a * (1 - a - a) * (1 - g)) / g) * ((1 - (1 - a) * (1 - g) * (2 / (1 + Math.sqrt(1 - 4 * (1 - g) * (1 - a) * a)))) / (1 - a + (1 - a) * a - a));
             var revenueRatio = Math.floor((totalPublished + initialPoint) / 2016) < 1 ? (a - (((1 - a) * a * (1 - a - a) * (1 - g)) / g) * ((1 - (1 - a) * (1 - g) * (2 / (1 + Math.sqrt(1 - 4 * (1 - g) * (1 - a) * a)))) / (1 - a + (1 - a) * a - a))) * b / t :
                     (a * ((1 - a + (1 - a) * a - a * a) / (1 - a + (1 - a) * a - a)) - (((1 - a) * a * (1 - a - a) * (1 - g)) / (g)) * ((1 - (1 - a) * (1 - g) * (2 / (1 + Math.sqrt(1 - 4 * (1 - g) * (1 - a) * a)))) / (1 - a + (1 - a) * a - a))) * b / t;
